8c60ab41a712664e8bb54e50d8529e7b5d2e7bf4
Niraj Sriramula <sniraj123@gmail.com>
2024-05-25 00:09:18 -0500

    Minimax agent with 1 look ahead done.

--- bot.py
+++ bot.py
@@ -1,1 +1,85 @@
-import chess
+from util import potential_check, map_images_with_pieces
+import random
+from chess_piece import Piece
+
+class Bot:
+    def __init__(self, color) -> None:
+        self.color = color
+        self.score = 0
+        self.piece_images = map_images_with_pieces()
+
+    def apply_move(self, king_Pos, selected_move, board, row, col, piece, score):
+        if piece.type == 10:
+            tup = (selected_move[1], selected_move[0])
+            king_Pos[1-piece.color] = tup
+        exPiece = board[selected_move[1]][selected_move[0]]
+        score += pow(-1, exPiece.color)*exPiece.type
+        board[row][col] = Piece(0,-1,None)
+        board[selected_move[1]][selected_move[0]] = piece
+        if piece.type == 1 and selected_move[1]==7*(1-piece.color):
+            board[selected_move[1]][selected_move[0]] = Piece(8, piece.color, self.piece_images['wQ' if piece.color==1 else 'bQ'])
+        self.score = score
+        return score
+    def copy_board(self, board):
+        score = 0
+        temp_board = [[Piece(0,-1,None) for x in range(8)] for y in range(8)]
+        for a in range(8):
+            for b in range(8):
+                temp_board[a][b] = board[a][b]
+                score+=(-1)*pow(-1, board[a][b].color)*board[a][b].type
+        return temp_board, score
+    
+    def get_pieces(self, board):
+        pieces = []
+        for a in range(8):
+            for b in range(8):
+                if board[a][b].color == self.color:
+                    pieces.append((board[a][b], b, a))
+        return pieces
+    def pick_best_move(self, board, king_Pos, pieces): 
+        best_move = None
+        _, curScore = self.copy_board(board)
+        best_pieces = []
+        best_moves = []
+        for index in range(len(pieces)):
+            piece = pieces[index][0]
+            potential_moves = piece.get_possible_valid_moves(pieces[index][1],pieces[index][2], board)
+            final_moves = []
+            for move in potential_moves:
+                if not potential_check(move, self.color==1, board, king_Pos, pieces[index][2],pieces[index][1]):
+                    final_moves.append(move)
+            if len(final_moves)==0:
+                continue
+            temp_board, curScore = self.copy_board(board) 
+            best_score = float('-inf') if piece.color == 1 else float('inf')
+            best_move = final_moves[0]
+            for move in final_moves:
+                curScore = self.apply_move(king_Pos, move, temp_board, pieces[index][2], pieces[index][1], piece, curScore)
+                if piece.color == 1:
+                    if curScore > best_score:
+                        best_score = curScore
+                        best_moves.append(move)
+                        best_pieces.append(pieces[index])
+                    elif curScore==best_score:
+                        best_pieces.append(pieces[index])
+                        best_moves.append(move)
+                else:
+                    if curScore < best_score:
+                        best_score = curScore
+                        best_moves = []
+                        best_pieces = []
+                        best_moves.append(move)
+                        best_pieces.append(pieces[index])
+                    elif curScore==best_score:
+                        best_pieces.append(pieces[index])
+                        best_moves.append(move)
+        
+        if len(best_moves)!=0:
+            print(len(best_pieces))
+            ind = random.randint(0, len(best_moves)-1)
+            self.apply_move(king_Pos, best_moves[ind], board, best_pieces[ind][2], best_pieces[ind][1], best_pieces[ind][0], curScore)
+        return best_move
+    
+    def make_move(self, board, king_Pos):
+        pieces = self.get_pieces(board)
+        self.pick_best_move(board, king_Pos, pieces)         
--- chess.py
+++ chess.py
@@ -1,27 +1,13 @@
 from sys import exit    
 from chess_piece import Piece
 import pygame
+from bot import Bot
+from util import potential_check, isCheck, isCheckMate, map_images_with_pieces, isStaleMate
 width = 700
 height = 700
 brown = (185,222,87)
 selected_color = (175,222,188)
-piece_images = {}
-
-def map_images_with_pieces():
-    piece_images["wP"] = pygame.image.load("assets/white_pawn.png")
-    piece_images["bP"] = pygame.image.load("assets/black_pawn.png")
-    piece_images["wR"] = pygame.image.load("assets/white_rook.png")
-    piece_images["bR"] = pygame.image.load("assets/black_rook.png")
-    piece_images["wB"] = pygame.image.load("assets/white_bishop.png")
-    piece_images["bB"] = pygame.image.load("assets/black_bishop.png")
-    piece_images["wK"] = pygame.image.load("assets/white_knight.png")
-    piece_images["bK"] = pygame.image.load("assets/black_knight.png")
-    piece_images["wKing"] = pygame.image.load("assets/white_king.png")
-    piece_images["bKing"] = pygame.image.load("assets/black_king.png")
-    piece_images["wQ"] = pygame.image.load("assets/white_queen.png")
-    piece_images["bQ"] = pygame.image.load("assets/black_queen.png")
-    piece_images["move"] = pygame.image.load("assets/move.png")
-
+piece_images = map_images_with_pieces()
 def draw_board():
     for row in range(8):
         for col in range(8):
@@ -70,25 +56,24 @@ def draw_potential_moves(moves):
    for move in moves:
        draw_potential_move(move[1], move[0])
 
-def select_piece():
-    global whiteChance, board, selected_tile
+def select_piece(selected_tile):
+    global whiteChance, board, tileSelected
     final_moves = []
     if board[selected_tile[1]][selected_tile[0]].type!=0:
         if (whiteChance == True and board[selected_tile[1]][selected_tile[0]].color==1) or (whiteChance==False and board[selected_tile[1]][selected_tile[0]].color==0):
-            draw_board()
             select_tile(selected_tile[1], selected_tile[0])
             potential_moves = board[selected_tile[1]][selected_tile[0]].get_possible_valid_moves(selected_tile[0],selected_tile[1], board)
             for move in potential_moves:
                 if not potential_check(move, whiteChance, board, king_Pos, selected_tile[1],selected_tile[0]):
                     final_moves.append(move)
-            draw_potential_moves(final_moves)
-        return True, final_moves
+        tileSelected = True
+        return final_moves
     else:
-        draw_board()
-        return False, None
-
+        tileSelected = False
+        return []
+    
 def make_move(final_moves, selected_move):
-    global checkMate, whiteChance, king_Pos, selected_tile, board
+    global checkMate, whiteChance, king_Pos, selected_tile, board, tileSelected
     if selected_move in final_moves and board[selected_tile[1]][selected_tile[0]].color!= board[selected_move[1]][selected_move[0]].color:
         piece = board[selected_tile[1]][selected_tile[0]]
         if piece.type == 10:
@@ -98,74 +83,12 @@ def make_move(final_moves, selected_move):
         board[selected_move[1]][selected_move[0]] = piece
         if piece.type == 1 and selected_move[1]==7*(1-piece.color):
             board[selected_move[1]][selected_move[0]] = Piece(8, piece.color, piece_images['wQ' if piece.color==1 else 'bQ'])
-        draw_board()
-        if isCheck(whiteChance, board, king_Pos):
-            p = "white" if whiteChance else "black"
-            print("Check caused by: " + p)
-            if isCheckMate(not whiteChance, board, king_Pos):
-                checkMate = 2 if whiteChance else 1
-                print("Checkmate bro: " + p + " wins")
         whiteChance = not whiteChance
-    return False, whiteChance 
-
-def isCheck(whiteChance, board, king_pos):
-    color = 0
-    pos = king_pos[0]
-    if whiteChance:
-        color = 1
-        pos = king_pos[1]
-    for i  in range(8):
-        for j in range(8):
-            if board[i][j].color==color:
-               moves = board[i][j].get_possible_valid_moves(j, i, board)
-               for move in moves:
-                  if move[0]==pos[1] and pos[0]==move[1]:
-                    return True
-    return False
-
-def isCheckMate(whiteChance, board, king_pos):
-    color = 0
-    pos = king_pos[0]
-    if whiteChance:
-        color = 1
-        pos = king_pos[1]
-    for i  in range(8):
-        for j in range(8):
-            if board[i][j].color==color:
-               moves = board[i][j].get_possible_valid_moves(j, i, board)
-               for move in moves:
-                    temp_board = [[Piece(0,-1,None) for x in range(8)] for y in range(8)]
-                    for a in range(8):
-                        for b in range(8):
-                            temp_board[a][b] = board[a][b]
-                    temp_board[move[1]][move[0]] = temp_board[i][j]
-                    temp_king_pos = [king_pos[o] for o in range(2)]
-                    if temp_board[i][j].type == 10:
-                        tup = (move[1], move[0])
-                        temp_king_pos[1-temp_board[i][j].color] = tup
-                    temp_board[i][j] = Piece(0,-1,None)
-                    if not isCheck( not whiteChance, temp_board, temp_king_pos):
-                        return False
-    return True
-
-def potential_check(move, whiteChance, board, king_pos, i, j):
-    temp_board = [[Piece(0,-1,None) for x in range(8)] for y in range(8)]
-    for a in range(8):
-       for b in range(8):
-          temp_board[a][b] = board[a][b]
-    temp_board[move[1]][move[0]] = temp_board[i][j]
-    temp_king_pos = [king_pos[o] for o in range(2)]
-    if temp_board[i][j].type == 10:
-       tup = (move[1], move[0])
-       temp_king_pos[1-temp_board[i][j].color] = tup
-    temp_board[i][j] = Piece(0,-1,None)
-    if isCheck( not whiteChance, temp_board, temp_king_pos):
-       return True
-    return False
-
+    tileSelected = False
+    return board
+staleMate = False
 def end_screen():
-    global checkMate, board
-    
+    global checkMate, board, staleMate
     winner = 'Black' if checkMate==1 else 'White'
     while True:
         for event in pygame.event.get():
@@ -174,7 +97,6 @@ def end_screen():
                 exit()
             if event.type ==pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    print("Restart")
                     board = [[Piece(5, 0, piece_images['bR']),Piece(3, 0, piece_images['bK']),
                         Piece(4, 0, piece_images['bB']),Piece(8, 0, piece_images['bQ']),
                         Piece(10, 0, piece_images['bKing']),Piece(4, 0, piece_images['bB']),
@@ -212,10 +134,15 @@ def end_screen():
                     draw_board()
                     return
         largeFont = pygame.font.SysFont('comicsans', 40)
-        winnerDisplay = largeFont.render(winner + ' Won the game!!', 1, (255, 255, 255))
+        color = (255, 255, 255) if winner=='White' else (0, 0, 0)
+        if staleMate:
+            drawDisplay = largeFont.render("Draw by stalemate", 1, (255,255,255))
+            screen.blit(drawDisplay, (width/2-drawDisplay.get_width()/2, 200))
+        else:
+            winnerDisplay = largeFont.render(winner + ' Won the game!!', 1, color)
+            screen.blit(winnerDisplay, (width/2-winnerDisplay.get_width()/2, 200))
         largeFont = pygame.font.SysFont('comicsans', 30)
         restart = largeFont.render('Press Enter to restart the game', 1, (255, 255, 255))
-        screen.blit(winnerDisplay, (width/2-winnerDisplay.get_width()/2, 200))
         screen.blit(restart, (width/2-restart.get_width()/2, 300))
         pygame.display.update()
         clock.tick(60)
@@ -253,20 +180,24 @@ board = [[Piece(5, 0, piece_images['bR']),Piece(3, 0, piece_images['bK']),
           Piece(4, 1, piece_images['wB']),Piece(8, 1, piece_images['wQ']),
           Piece(10, 1, piece_images['wKing']),Piece(4, 1, piece_images['wB']),
           Piece(3, 1, piece_images['wK']),Piece(5, 1, piece_images['wR'])]]
-pygame.init()
-screen = pygame.display.set_mode((width,height))
 black = (94,94,94)
-clock = pygame.time.Clock()
 selected_tile = ()
 prev_tile = ()
 tileSelected = False
 potential_moves = []
 whiteChance = True
-draw_board()
 king_Pos = [(7,4),(0,4)]
 checkMate = 0
+clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((width,height))
+draw_board()
+black_bot = Bot(0)
+white_bot = Bot(1)
+number_of_moves = 0
 while True:
     if checkMate>0:
+        number_of_moves = 0
         end_screen()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -275,12 +206,39 @@ while True:
         if event.type ==pygame.MOUSEBUTTONDOWN:
             if tileSelected:
                 selected_move = get_clicked_tile()
-                tileSelected, whiteChance = make_move(final_moves, selected_move)
+                make_move(final_moves, selected_move)
+                if isCheck(not whiteChance, board, king_Pos):
+                    if isCheckMate(whiteChance, board, king_Pos):
+                        checkMate = 1 if whiteChance else 2
+                draw_board()
                 prev_tile = selected_tile
             if len(prev_tile)==2:
                 unselect_tile(prev_tile[1], prev_tile[0])
             selected_tile = get_clicked_tile()
-            tileSelected, final_moves = select_piece()
+            final_moves = select_piece(selected_tile)
+            draw_potential_moves(final_moves)
+    if not whiteChance:
+        black_bot.make_move(board, king_Pos)
+        whiteChance = not whiteChance
+        if isStaleMate(not whiteChance, board, king_Pos):
+            staleMate = True
+            end_screen()
+        if isCheck(not whiteChance, board, king_Pos):
+            if isCheckMate(whiteChance, board, king_Pos):
+                checkMate = 1 if whiteChance else 2
+        number_of_moves+=1
+        draw_board()
+    if whiteChance:
+        white_bot.make_move(board, king_Pos)
+        whiteChance = not whiteChance
+        if isStaleMate(not whiteChance, board, king_Pos):
+            staleMate = True
+            end_screen()
+        if isCheck(not whiteChance, board, king_Pos):
+            if isCheckMate(whiteChance, board, king_Pos):
+                checkMate = 1 if whiteChance else 2
+        number_of_moves+=1
+        draw_board()
     draw_pieces(board)
     pygame.display.update()
     clock.tick(60)
